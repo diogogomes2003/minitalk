2a5bc2f3d1135e06873557a166fd3c3c6c97341c
diogogomes2003 <teste123456789teste1234567890@gmail.com>
2023-06-12 09:31:24 +0100

    add server.c and client.c

--- server.c
+++ server.c
@@ -11,3 +11,44 @@
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	sigusr_handler(int signal)
+{
+	static int				bit_server = 0;
+	static unsigned char	c = 0;
+
+	if (signal == SIGUSR1)
+	{
+		c = c | (0x01 << bit_server);
+		bit_server++;
+	}
+	else if (signal == SIGUSR2)
+		bit_server++;
+	if (bit_server == 8)
+	{
+		ft_printf("%c", c);
+		bit_server = 0;
+		c = 0;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	pid;
+
+	(void)av;
+	if (ac != 1)
+	{
+		ft_printf("Try <./server>\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("Server PID: %i\n", pid);
+	ft_printf("Waiting messagem from client...\n");
+	while (ac == 1)
+	{
+		signal(SIGUSR1, sigusr_handler);
+		signal(SIGUSR2, sigusr_handler);
+	}
+	return (0);
+}
